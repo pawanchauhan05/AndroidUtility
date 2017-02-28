1ed39c90f51b4ca7e8437d31c779f2a94653d1fb
pawan singh chauhan <pawansinghchouhan052gmail.com>
2017-02-28 17:28:55 +0530

    circular image view added

--- app/src/main/java/com/androidutility/activity/MainActivity.java
+++ app/src/main/java/com/androidutility/activity/MainActivity.java
@@ -1,10 +1,14 @@
 package com.androidutility.activity;
 
 import android.Manifest;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import com.android.utility.CircularImageView;
 import com.android.utility.RunTimePermission;
 import com.android.utility.RuntimePermissionInterface;
 import com.android.utility.Utils;
@@ -16,11 +20,15 @@ public class MainActivity extends AppCompatActivity {
     public RunTimePermission permission;
     private String url = "http://www.maninblack.org/demos/WhereDoAllTheJunkiesComeFrom.mp3";
     private String musicFileName = "music";
+    private CircularImageView circularImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        circularImageView = (CircularImageView) findViewById(R.id.circularImageView);
+        Bitmap icon = BitmapFactory.decodeResource(getResources(),R.drawable.pp);
+        circularImageView.setImageBitmap(icon);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.frameLayout, new TestFragment())
